562cde3c3327b06acbe5ee9ee89f2894e1b04b7f
Colin Richardson <richardsonjcolin@gmail.com>
2020-09-09 03:27:31 +0000

    fetch data from db for our root route

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,9 @@
 get '/' do
-  File.read(File.join('app/views', 'index.html'))
+  @finstagram_posts = FinstagramPost.order(created_at: :desc)
+  erb(:index)
+end
+
+
+get '/lecture' do
+  File.read(File.join('app/views', 'index-example.html'))
 end
