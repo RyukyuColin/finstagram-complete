153c74c15fe9594215ff7a3188364afd70a621e0
Colin Richardson <mail@colinrichardson.ca>
2021-02-17 21:55:34 +0000

    remove migration file

--- db/migrate/1_rename_password_column.rb
+++
@@ -1,9 +0,0 @@
-class RenamePasswordColumn < ActiveRecord::Migration[5.2]
-  def up
-    rename_column :users, :password, :password_digest
-  end
-
-  def down
-    rename_column :users, :password_digest, :password
-  end
-end
